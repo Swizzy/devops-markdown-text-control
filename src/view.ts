1af6d6813307c522b2665c27a43a6adee7ebceb1
Swizzy <swizzy@xeupd.com>
2023-05-18 12:41:23 +0200

    Removed the input field, we don't need it.

--- src/view.ts
+++ src/view.ts
@@ -30,33 +30,11 @@ export class View {
         var workItemControl = $('<div />');
         workItemControl.addClass('work-item-control');
 
-        this._combo = $('<div />');
-        this._combo.addClass('combo');
-        this._combo.addClass('input-text-box');
-
-        var wrap = $("<div />");
-        wrap.addClass("wrap");
-
         this.currentValue = String(this.model.getCurrentValue());
-
-        var field = $("<input />").attr("type", "text");
-        field.val(this.currentValue);
-        field.attr("autocomplete", this.currentValue);
-        field.attr("aria-valuenow", this.currentValue);
-        field.on("keyup", (evt: JQueryKeyEventObject) => {
-            this._inputChanged();
-        }).on('focus', (evt: JQueryKeyEventObject) => {
-            this._gotFocus();
-        }).on('blur', (evt: JQueryKeyEventObject) => {
-            this._lostFocus();
-        });
 
         this._markdown = $('<div />');
         this._markdown.addClass('markdown-control');
 
-        wrap.append(field);
-        this._combo.append(wrap);
-        workItemControl.append(this._combo);
         control.append(workItemControl);
         control.append(this._markdown);
         container.append(control);
