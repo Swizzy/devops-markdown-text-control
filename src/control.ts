b0314025ceea752f100d1f0b267103c3c33024f2
Swizzy <swizzy@xeupd.com>
2023-05-18 12:12:32 +0200

    Updated things to not require the markdown ahead of time in order to function (hopefully?)

--- src/control.ts
+++ src/control.ts
@@ -10,8 +10,6 @@ import * as Q from "q";
 
 export class Controller {
     private _fieldName = "";
-    private _markdown = "";
-    private _script = "";
 
     private _inputs: IDictionaryStringTo<string>;
     private _model: Model;
@@ -21,11 +19,10 @@ export class Controller {
         this._initialize();
     }
 
-    private _initialize(): void {
+    private async _initialize(): Promise<void> {
         this._inputs = VSS.getConfiguration().witInputs;
         this._fieldName = this._inputs["FieldName"];
-        this._markdown = this._inputs["Markdown"];
-        this._script = this._inputs["Script"];
+        var markdown = await this.getFieldValue(this._fieldName);
 
         WitService.WorkItemFormService.getService().then(
             (service) => {
@@ -36,18 +33,42 @@ export class Controller {
                         
                         // dependent on view, model, and inputParser refactoring
                         this._model = new Model(currentValue);
-                        this._view = new View(this._model, this._script, (val) => {
+                        this._view = new View(this._model, "", (val) => {
                             this._updateInternal(val);
                         });
                         
                         //Force update markdown after view is created.
-                        this._view.update(currentValue, this._markdown);
+                        this._view.update(currentValue, markdown);
 
                     }, this._handleError
                 ).then(null, this._handleError);
             },
             this._handleError);
+        
     }
+
+    private async getFieldValue(fieldName: string): Promise<string> {
+        const formService = await WitService.WorkItemFormService.getService();
+        fieldName = fieldName.toLowerCase()
+
+        if (fieldName == "id") {
+            return formService.getId();
+        }
+
+        try {
+            const fields = await formService.getFields();
+            const field = fields.filter(x => x.name.toLowerCase() == fieldName)[0];
+
+            if (field) {
+                return await formService.getFieldValue(field.referenceName);
+            } else {
+                return null;
+            }
+        } catch {
+            return null;
+        }
+    }
+
 
     private _handleError(error: string): void {
         new ErrorView(error);
@@ -65,9 +86,9 @@ export class Controller {
         );
     }
 
-    private _update(value: string): void {
+    private async _update(value: string): Promise<void> {
         this._model.setCurrentValue(value);
-        this._view.update(value, this._markdown);
+        this._view.update(value, await this.getFieldValue(this._fieldName));
     }
 
     public updateExternal(value: string): void {
