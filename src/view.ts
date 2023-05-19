f9c8b7916e23d77b9e9ad2f8c244582fa92eec97
Swizzy <swizzy@xeupd.com>
2023-05-19 09:03:16 +0200

    Added TextArea to begin getting full support for everything and fixed styling for code elements

--- src/view.ts
+++ src/view.ts
@@ -3,6 +3,7 @@ import * as MarkdownIt from "markdown-it";
 export class View {
     private _md;
     private _markdown: JQuery<HTMLElement>;
+    private _input: JQuery<HTMLElement>;
 
     constructor() {
         this._md = new MarkdownIt({ html: true, breaks: true });
@@ -12,8 +13,14 @@ export class View {
         var container = $("<div />");
         container.addClass("mdcontainer");
 
+        this._input = $('<textarea rows="10" />');
+        this._input.on('input', this._oninput)
+        
+        container.append(this._input);
+
         var control = $('<div />');
         control.addClass('control');
+
         this._markdown = $('<div />');
         this._markdown.addClass('markdown-control');
 
@@ -23,8 +30,16 @@ export class View {
         VSS.resize();
     }
 
+    public _oninput(e:Event) {
+        this._update(this._input.val().toString());
+    }
 
-    public update(markdown: string) {
+    public update(markdown: string) { 
+        this._input.val(markdown);
+        this._update(markdown);
+    }
+
+    private _update(markdown: string) {
         console.log('update: ' + markdown);
         markdown = $("<div/>").html(markdown.replaceAll("<br>", "\\n").replaceAll("&nbsp;", " ")).text().replaceAll("\\n", "\n");
         console.log('stripped: ' + markdown);
